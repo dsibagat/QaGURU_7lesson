64f1a08b95e2bf17cf1c35aaaf8af00b492512e3
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-02-17 14:50:53 +0300

    add test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -7,9 +8,17 @@ import static org.hamcrest.Matchers.is;
 
 public class SimpleTest {
     @Test
+    @Tag("positive")
     @DisplayName("Some positive test")
     void successWithHamcrestAndStepsTest() {
         step("Assert that true is true", () ->
                 assertThat(true, is(true)));
+    }
+
+    @Test
+    @DisplayName("Some negative test")
+    void negativeWithHamcrestAndStepsTest() {
+        step("Assert that true is false", () ->
+                assertThat(true, is(false)));
     }
 }
